0c5cc542a5104fd71b4bf195acb3a77ac5abe2bb
Anton <a.chertok@akatech.com.ua>
2024-10-25 00:24:01 +0100

    refactor one post styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,18 @@ import { PostType } from "@/types/entities";
 import { Pageable } from "@/types/responses";
 
 const Home = async () => {
- const response = await basicFetch<Pageable<PostType>>("/posts");
- console.log(response);
+  const response = await basicFetch<Pageable<PostType>>("/posts");
+  console.log(response);
 
- return (
-  <>
-   <Container>
-    <h1 className="text-4xl mb-8">Posts</h1>
+  return (
+    <>
+      <Container>
+        <h1 className="text-4xl mb-8">Posts</h1>
 
-    {response?.data && <PostList list={response.data} />}
-   </Container>
-  </>
- );
+        {response?.data && <PostList list={response.data} />}
+      </Container>
+    </>
+  );
 };
 
 export default Home;
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,3 +1,4 @@
+import "./Post.style.css";
 import Image from "next/image";
 import parse from "html-react-parser";
 import { FC } from "react";
@@ -7,31 +8,31 @@ import { PostType } from "@/types/entities";
 import { createImageApiUrl } from "@/config/url";
 
 interface IProps {
- data: PostType;
+  data: PostType;
 }
 
 const Post: FC<IProps> = ({ data: { banner, title, content } }) => {
- return (
-  <>
-   <Container>
-    <article>
-     <h1 className="text-4xl font-bold mb-6">{title}</h1>
-     <div className="min-h-[400px] max-w-[700px] overflow-hidden relative rounded-2xl mb-6">
-      <Image
-       priority
-       className="h-full w-full object-cover"
-       fill
-       src={createImageApiUrl(banner)}
-       alt={title}
-       sizes="30vw"
-       quality={75}
-      />
-     </div>
-     {content && <div className="content">{parse(content)}</div>}
-    </article>
-   </Container>
-  </>
- );
+  return (
+    <>
+      <Container>
+        <article>
+          <h1 className="post-heading">{title}</h1>
+          <div className="post-image-wrapper">
+            <Image
+              priority
+              className="post-image"
+              fill
+              src={createImageApiUrl(banner)}
+              alt={title}
+              sizes="30vw"
+              quality={75}
+            />
+          </div>
+          {content && <div className="content">{parse(content)}</div>}
+        </article>
+      </Container>
+    </>
+  );
 };
 
 export default Post;
