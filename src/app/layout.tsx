a55da7215d49c15319058463d362f19432f3afbf
Anton <a.chertok@akatech.com.ua>
2024-10-25 01:51:21 +0100

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,39 +5,33 @@ import { Open_Sans } from "next/font/google";
 // import localFont from "next/font/local";
 
 const opensans = Open_Sans({
- weight: ["400", "700"],
- style: ["normal"],
- subsets: ["cyrillic"],
- display: "swap",
- variable: "--font-opensans",
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["cyrillic"],
+  display: "swap",
+  variable: "--font-opensans",
 });
 
-// const geistSans = localFont({
-//  src: "./fonts/GeistVF.woff",
-//  variable: "--font-geist-sans",
-//  weight: "100 900",
-// });
-// const geistMono = localFont({
-//  src: "./fonts/GeistMonoVF.woff",
-//  variable: "--font-geist-mono",
-//  weight: "100 900",
-// });
-
 export const metadata: Metadata = {
- title: "Sinova test task",
- description: "Description - Sinova test task",
+  title: "Sinova test task",
+  description: "Description - Sinova test task",
 };
 
 export default function RootLayout({
- children,
+  children,
 }: Readonly<{
- children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
- return (
-  <html lang="en">
-   <body className={`${opensans.variable} antialiased`}>
-    <section className="py-10">{children}</section>
-   </body>
-  </html>
- );
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/android-icon-192x192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="./apple-icon-180x180.png" sizes="180x180" />
+      </head>
+
+      <body className={`${opensans.variable} antialiased`}>
+        <section className="py-10">{children}</section>
+      </body>
+    </html>
+  );
 }
